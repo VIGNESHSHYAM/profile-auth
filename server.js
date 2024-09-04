41e0bac9b8a5268a0a686a1afcb52b5723dd7e77
VIGNESHSHYAM <126313145+VIGNESHSHYAM@users.noreply.github.com>
2024-09-04 16:46:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const server_1 = require("@trpc/server");
 const trpcExpress = __importStar(require("@trpc/server/adapters/express"));
 const router_1 = require("./router");
 const db_1 = __importDefault(require("./db")); // Import the DB connection
+const cors_1 = __importDefault(require("cors"));
 const createContext = ({ req, res }) => ({
     req,
     res,
@@ -40,6 +41,10 @@ const createContext = ({ req, res }) => ({
 const t = server_1.initTRPC.context().create();
 const app = (0, express_1.default)();
 (0, db_1.default)();
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    credentials: true, // Allow cookies to be sent
+}));
 app.use((0, cookie_session_1.default)({
     name: 'session',
     secret: 'your-secret-key',
@@ -54,6 +59,6 @@ app.use('/trpc', trpcExpress.createExpressMiddleware({
     router: router_1.appRouter,
     createContext,
 }));
-app.listen(10000, () => {
+app.listen(4000, () => {
     console.log('Server is running on http://localhost:4000');
 });
